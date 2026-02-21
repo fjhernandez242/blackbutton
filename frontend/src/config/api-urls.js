f0624b68c95e428089e2e846d906a753a05bac5f
fjhernandez242 <fjhernandez049@gmail.com>
2026-02-20 19:10:52 -0600

    -Se hace referencia a la Django Rest-Framework con el subdominio. -Se generar carpeta dist

--- frontend/src/config/api-urls.js
+++ frontend/src/config/api-urls.js
@@ -1,5 +1,5 @@
 // Url base
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://blackbuttonbackend.fjdev.icu';
 // Urls endpoint
 export const URLS = {
     // Obtiene todos los usuarios
